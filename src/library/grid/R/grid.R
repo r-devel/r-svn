r-devel/r-svn

commit 8ce2cd97ca976ba31b4502409bfc78671ae8b8bf

simplify some manipulations of environments; more to come
git-svn-id: https://svn.r-project.org/R/trunk@67698 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/grid/R/grid.R
@@ -287,20 +287,12 @@ current.parent <- function(n=1) {
 }
 
 vpListFromNode <- function(node) {
-  childnames <- names(node$children)
-  n <- length(childnames)
-  children <- vector("list", n)
-  index <- 1
-  for (i in childnames) {
-    children[[index]] <- vpTreeFromNode(get(i, envir=node$children))
-    index <- index + 1
-  }
-  vpListFromList(children)
+  vpListFromList(as.list(node$children, all.names=TRUE))
 }
 
 vpTreeFromNode <- function(node) {
   # If no children then just return viewport
-  if (length(names(node$children)) == 0)
+  if (no.children(node$children))
     vpFromPushedvp(node)
   # Otherwise return vpTree
   else

src/library/grid/R/grab.R
@@ -29,7 +29,7 @@ rootVP <- function(pvp) {
 # List the children of the current vp (as a vpList)
 current.vpList <- function() {
   cpvp <- grid.Call(L_currentViewport)
-  if (length(names(cpvp$children)) == 0)
+  if (no.children(cpvp$children))
     NULL
   else
     vpListFromNode(cpvp)
@@ -45,7 +45,7 @@ vpExists <- function(vp) {
 }
 
 vpExists.viewport <- function(vp) {
-  vp$name %in% names(.Call(L_currentViewport)$children)
+  exists(vp$name, .Call(L_currentViewport)$children)
 }
 
 vpExists.vpStack <- function(vp) {
@@ -53,7 +53,7 @@ vpExists.vpStack <- function(vp) {
 }
 
 vpExists.vpList <- function(vp) {
-  any(sapply(vp, vpExists, simplify=TRUE))
+  any(vapply(vp, vpExists, logical(1L), simplify=TRUE))
 }
 
 vpExists.vpTree <- function(vp) {

src/library/methods/R/makeBasicFunsList.R
@@ -39,7 +39,7 @@ utils::globalVariables(".addBasicGeneric")
 
     ## Next, add the remaining primitive generics
     prims <- names(.GenericArgsEnv)
-    new_prims <- prims[!prims %in% names(funs)]
+    new_prims <- setdiff(prims, names(funs))
     for(nm in new_prims) {
         f <- get(nm, envir = .GenericArgsEnv)
         body(f) <- substitute(standardGeneric(ff), list(ff=val))
@@ -47,9 +47,9 @@ utils::globalVariables(".addBasicGeneric")
     }
 
     ## Then add all the primitives that are not already there.
-    ff <- names(BE <- baseenv())
-    prims <- ff[vapply(ff, function(x) is.primitive(BE[[x]]), NA)]
-    new_prims <- prims[!prims %in% names(funs)]
+    ff <- as.list(baseenv(), all.names=TRUE)
+    prims <- ff[vapply(ff, is.primitive, logical(1L))]
+    new_prims <- setdiff(names(prims), names(funs))
     add <- rep(list(FALSE), length(new_prims))
     names(add) <- new_prims
     funs <- c(funs, add)

src/library/tools/R/utils.R
@@ -1296,9 +1296,8 @@ function(parent = parent.frame(), fixup = FALSE)
 {
     ## Create an environment with pseudo-definitions for the S3 primitive
     ## generics
-    env <- new.env(hash = TRUE, parent = parent)
-    for(f in names(base::.GenericArgsEnv))
-        assign(f, get(f, envir=base::.GenericArgsEnv), envir = env)
+    env <- list2env(as.list(base::.GenericArgsEnv, all.names=TRUE),
+                    hash=TRUE, parent=parent)
     if(fixup) {
         ## now fixup the operators
         for(f in c('+', '-', '*', '/', '^', '%%', '%/%', '&', '|',
@@ -1318,10 +1317,8 @@ function(parent = parent.frame())
 {
     ## Create an environment with pseudo-definitions
     ## for the S3 primitive non-generics
-    env <- new.env(hash = TRUE, parent = parent)
-    for(f in names(base::.ArgsEnv))
-        assign(f, get(f, envir=base::.ArgsEnv), envir = env)
-    env
+    list2env(as.list(base::.ArgsEnv, all.names=TRUE),
+             hash=TRUE, parent=parent)
 }
 
 ### ** .make_S3_methods_stop_list

src/library/utils/R/objects.R
@@ -31,9 +31,9 @@ function(fname, envir)
 	## maybe an S3 generic was turned into the S4 default
 	## Try to find it, otherwise warn :
 	fMethsEnv <- methods::getMethodsForDispatch(f)
-	r <- lapply(grep("^ANY\\b", names(fMethsEnv), value=TRUE),
-		    get, envir = fMethsEnv)
-	if(any(ddm <- unlist(lapply(r, class)) == "derivedDefaultMethod"))
+        meths <- as.list(fMethsEnv, all.names=TRUE)
+        r <- meths[grep("^ANY\\b", names(meths))]
+	if(any(ddm <- vapply(r, is, logical(1L), "derivedDefaultMethod")))
 	    f <- r[ddm][[1]]@.Data
 	else
 	    warning(gettextf(

commit 1100b4f5d054f15368522a3ebcb5ce49acaac578

fix bug introduced in r67698
git-svn-id: https://svn.r-project.org/R/trunk@67706 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/grid/R/grid.R
@@ -287,7 +287,7 @@ current.parent <- function(n=1) {
 }
 
 vpListFromNode <- function(node) {
-  vpListFromList(as.list(node$children, all.names=TRUE))
+  vpListFromList(eapply(node$children, vpTreeFromNode, all.names=TRUE))
 }
 
 vpTreeFromNode <- function(node) {