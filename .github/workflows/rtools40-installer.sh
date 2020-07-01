131563e69ded6aee14014b9eb7f2b2db855c5be4
Jeroen Ooms <jeroenooms@gmail.com>
2020-07-02 00:49:32 +0200

    Re-enable MiKTeX 20.6"
    This reverts commit 7ec2821c9d0b3ea5dd7256f57184256bb8b4cdcc.

--- .github/workflows/rtools40-installer.sh
+++ .github/workflows/rtools40-installer.sh
@@ -12,7 +12,7 @@ scripts=$(dirname $(realpath $0))
 sources=$(cygpath ${GITHUB_WORKSPACE})
 
 # Put pdflatex on the path (needed only for CMD check)
-export PATH="$PATH:/c/progra~1/git/bin:/c/progra~1/MiKTeX 2.9/miktex/bin/x64"
+export PATH="$PATH:/c/progra~1/git/bin:/c/progra~1/MiKTeX/miktex/bin/x64"
 echo "PATH: $PATH"
 pdflatex --version
 texindex --version
