r-devel/r-svn

commit c359055b179df080fac60c3fe453f95fa00ca42a

.Traceback w/o deparse()ing
git-svn-id: https://svn.r-project.org/R/trunk@76872 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/errors.c
@@ -295,7 +295,7 @@ static char *Rstrncat(char *dest, const char *src, size_t n)
     size_t before = strlen(dest);
 
     strncat(dest, src, n);
-    
+
     after = strlen(dest);
     if (after - before == n)
 	/* the string may have been truncated, but we cannot know for sure
@@ -350,7 +350,7 @@ void warning(const char *format, ...)
     va_start(ap, format);
     size_t psize;
     int pval;
-    
+
     psize = min(BUFSIZE, R_WarnLength+1);
     pval = Rvsnprintf(buf, psize, format, ap);
     va_end(ap);
@@ -1008,7 +1008,7 @@ static void jump_to_top_ex(Rboolean traceback,
 	   (which should not happen) */
 	if (traceback && inError < 2 && inError == oldInError) {
 	    inError = 2;
-	    PROTECT(s = R_GetTraceback(0));
+	    PROTECT(s = R_GetTracebackOnly(0));
 	    SET_SYMVALUE(install(".Traceback"), s);
 	    /* should have been defineVar
 	       setVar(install(".Traceback"), s, R_GlobalEnv); */
@@ -1440,9 +1440,11 @@ static void R_PrintDeferredWarnings(void)
 	PrintWarnings();
     }
 }
-
+/*
+ * Return the traceback without deparsing the calls
+ */
 attribute_hidden
-SEXP R_GetTraceback(int skip)
+SEXP R_GetTracebackOnly(int skip)
 {
     int nback = 0, ns;
     RCNTXT *c;
@@ -1467,7 +1469,7 @@ SEXP R_GetTraceback(int skip)
 	    if (skip > 0)
 		skip--;
 	    else {
-		SETCAR(t, deparse1m(c->call, 0, DEFAULTDEPARSE));
+		SETCAR(t, duplicate(c->call));
 		if (c->srcref && !isNull(c->srcref)) {
 		    SEXP sref;
 		    if (c->srcref == R_InBCInterpreter)
@@ -1481,6 +1483,25 @@ SEXP R_GetTraceback(int skip)
 	}
     UNPROTECT(1);
     return s;
+}
+/*
+ * Return the traceback with calls deparsed
+ */
+attribute_hidden
+SEXP R_GetTraceback(int skip)
+{
+    int nback = 0;
+    SEXP s, t, u, v;
+    s = PROTECT(R_GetTracebackOnly(skip));
+    for(t = s; t != R_NilValue; t = CDR(t)) nback++;
+    u = v = PROTECT(allocList(nback));
+
+    for(t = s; t != R_NilValue; t = CDR(t), v=CDR(v)) {
+        SETCAR(v, PROTECT(deparse1m(CAR(t), 0, DEFAULTDEPARSE)));
+        UNPROTECT(1);
+    }
+    UNPROTECT(2);
+    return u;
 }
 
 SEXP attribute_hidden do_traceback(SEXP call, SEXP op, SEXP args, SEXP rho)

src/gnuwin32/Rdll.hide
@@ -94,6 +94,7 @@
  R_GetMaxNSize
  R_GetMaxVSize
  R_GetTraceback
+ R_GetTracebackParsed
  R_GetVarLocSymbol
  R_GetVarLocValue
  R_HandlerStack

src/include/Defn.h
@@ -1295,7 +1295,8 @@ SEXP ItemName(SEXP, R_xlen_t);
 void NORET errorcall_cpy(SEXP, const char *, ...);
 void NORET ErrorMessage(SEXP, int, ...);
 void WarningMessage(SEXP, R_WARNING, ...);
-SEXP R_GetTraceback(int);
+SEXP R_GetTraceback(int);    // including deparse()ing
+SEXP R_GetTracebackOnly(int);// no        deparse()ing
 
 R_size_t R_GetMaxVSize(void);
 void R_SetMaxVSize(R_size_t);

src/library/base/R/traceback.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/traceback.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2016 The R Core Team
+#  Copyright (C) 1995-2019 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,17 +16,33 @@
 #  A copy of the GNU General Public License is available at
 #  https://www.R-project.org/Licenses/
 
-.traceback <- function(x = NULL) {
-    if(is.null(x) && !is.null(x <- get0(".Traceback", envir = baseenv())))
-	{}
-    else if (is.numeric(x))
+.traceback <- function(x = NULL,
+		       max.lines = getOption("traceback.max.lines",
+					     getOption("deparse.max.lines", -1L)))
+{
+    if((is.null(x) && !is.null(x <- get0(".Traceback", envir = baseenv()))) ||
+       ((is.pairlist(x) || is.list(x)) && all(vapply(x, typeof, "type") == "language")))
+       {
+        for(i in seq_along(x)) {
+            srcref <- attr(x[[i]], 'srcref')
+            x[[i]] <- deparse(x[[i]], nlines=max.lines)
+            attr(x[[i]], 'srcref') <- srcref
+        }
+    }
+    else if(is.numeric(x))
     	x <- .Internal(traceback(x))
     x
 }
 
-traceback <- function(x = NULL, max.lines = getOption("deparse.max.lines"))
+traceback <- function(x = NULL,
+                      max.lines = getOption("traceback.max.lines",
+                                            getOption("deparse.max.lines", -1L)))
 {
-    n <- length(x <- .traceback(x))
+    .is.positive.intlike <- function(x) is.numeric(x) && !is.na(x) && as.integer(x) > 0L
+    valid.max.lines <- .is.positive.intlike(max.lines)
+    if(!valid.max.lines)
+        max.lines <- -1L
+    n <- length(x <- .traceback(x, max.lines=max.lines))
     if(n == 0L)
         cat(gettext("No traceback available"), "\n")
     else {
@@ -40,7 +56,7 @@ traceback <- function(x = NULL, max.lines = getOption("deparse.max.lines"))
                 paste0(" at ", basename(srcfile$filename), "#", srcref[1L])
             }
             ## Truncate deparsed code (destroys attributes of xi)
-            if(is.numeric(max.lines) && max.lines > 0L && max.lines < m) {
+            if(valid.max.lines && max.lines < m) {
                 xi <- c(xi[seq_len(max.lines)], " ...")
                 m <- length(xi)
             }

src/library/base/man/options.Rd
@@ -125,8 +125,13 @@ getOption(x, default = NULL)
     }
 
     \item{\code{deparse.max.lines}:}{controls the number of lines used
-      when deparsing in \code{\link{traceback}}, \code{\link{browser}},
-      and upon entry to a function whose debugging flag is set.
+      when deparsing in \code{\link{browser}}, upon entry to a function
+      whose debugging flag is set, and if option \code{traceback.max.lines}
+      is unset, of \code{\link{traceback}()}.  Initially unset, and only
+      used if set to a positive integer.}
+
+    \item{\code{traceback.max.lines}:}{controls the number of lines used
+      when deparsing in \code{\link{traceback}}, if set.
       Initially unset, and only used if set to a positive integer.}
 
     \item{\code{digits}:}{controls the number of \emph{significant} (see \code{\link{signif}}) digits to

src/library/base/man/traceback.Rd
@@ -13,28 +13,30 @@
   uncaught error, i.e., the sequence of calls that lead to the error.
   This is useful when an error occurs with an unidentifiable error
   message.  It can also be used to print the current stack or
-  arbitrary lists of deparsed calls.
+  arbitrary lists of calls.
 
   \code{.traceback()} now \emph{returns} the above call stack (and
   \code{traceback(x, *)} can be regarded as convenience function for
   printing the result of \code{.traceback(x)}).
 }
 \usage{
-traceback(x = NULL, max.lines = getOption("deparse.max.lines"))
-.traceback(x = NULL)
+ traceback(x = NULL, max.lines = getOption("traceback.max.lines",
+                                           getOption("deparse.max.lines"))
+.traceback(x = NULL, max.lines = getOption("traceback.max.lines",
+                                           getOption("deparse.max.lines"))
 }
 \arguments{
   \item{x}{\code{NULL} (default, meaning \code{.Traceback}), or an
     integer count of calls to skip in the current stack, or a list or
-    pairlist of deparsed calls.  See the details.}
-  \item{max.lines}{The maximum number of lines to be printed
+    pairlist of calls.  See the details.}
+  \item{max.lines}{the maximum number of lines to be printed
     \emph{per call}.  The default is unlimited.}
 }
 \details{
   The default display is of the stack of the last uncaught error as
-  stored as a list of deparsed calls in \code{.Traceback}, which
+  stored as a list of \code{\link{call}}s in \code{.Traceback}, which
   \code{traceback} prints in a user-friendly format.  The stack of
-  deparsed calls always contains all function calls and all foreign
+  calls always contains all function calls and all foreign
   function calls (such as \code{\link{.Call}}): if profiling is in
   progress it will include calls to some primitive functions.  (Calls
   to builtins are included, but not to specials.)
@@ -48,15 +50,15 @@ traceback(x = NULL, max.lines = getOption("deparse.max.lines"))
   \code{x} entries at the top of the stack.  For example,
   \code{options(error = function() traceback(3))} will print the stack
   at the time of the error, skipping the call to \code{traceback()} and
-  \code{.traceback()} 
+  \code{.traceback()}
   and the error function that called it.
 
-  Otherwise, \code{x} is assumed to be a list or pairlist of deparsed
-  calls and will be displayed in the same way.
+  Otherwise, \code{x} is assumed to be a list or pairlist of calls and will
+  be displayed in the same way.
 }
 \value{
-  \code{traceback()} prints the deparsed call stack deepest call
-  first, and returns it invisibly.  The calls may print on more than
+  \code{traceback()} prints the call stack deepest call first, and
+  returns it invisibly.  The calls may print on more than
   one line, and the first line for each call is labelled by the frame
   number.  The number of lines printed per call can be limited via
   \code{max.lines}.

tests/reg-tests-2.Rout.save
@@ -1,5 +1,5 @@
 
-R Under development (unstable) (2019-07-17 r76847) -- "Unsuffered Consequences"
+R Under development (unstable) (2019-07-23 r76869) -- "Unsuffered Consequences"
 Copyright (C) 2019 The R Foundation for Statistical Computing
 Platform: x86_64-pc-linux-gnu (64-bit)
 
@@ -5092,7 +5092,6 @@ Calls: do.call -> <Anonymous>
 2: (function (...) 
    stop())(mpg = c(21, 21, 22.8, 21.4, 18.7, 18.1, 14.3, 24.4, 22.8, 
    19.2, 17.8, 16.4, 17.3, 15.2, 10.4, 10.4, 14.7, 32.4, 30.4, 33.9, 
-    ...
 1: do.call(f, mtcars)
 > 
 > ## Debugger can handle a function that has a single function call as its body

commit d3d8a575046b431d53e077408d70a585f885433c

copy srcref for .traceback()
Introduced regression in r76872 by failing to copy
srcref attribute for '.traceback(n)' case.

src/main/errors.c
@@ -1550,8 +1550,12 @@ SEXP R_GetTraceback(int skip)
     u = v = PROTECT(allocList(nback));
 
     for(t = s; t != R_NilValue; t = CDR(t), v=CDR(v)) {
-        SETCAR(v, PROTECT(deparse1m(CAR(t), 0, DEFAULTDEPARSE)));
-        UNPROTECT(1);
+	SEXP sref = getAttrib(CAR(t), R_SrcrefSymbol);
+	SEXP dep = PROTECT(deparse1m(CAR(t), 0, DEFAULTDEPARSE));
+	if (!isNull(sref))
+	    setAttrib(dep, R_SrcrefSymbol, duplicate(sref));
+	SETCAR(v, dep);
+	UNPROTECT(1);
     }
     UNPROTECT(2);
     return u;