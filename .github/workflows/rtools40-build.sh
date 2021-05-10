4d43ac49fdffb36c4674635c641111393c445fae
Jeroen Ooms <jeroenooms@gmail.com>
2021-05-11 00:04:47 +0200

    Add new urct64 target for Windows (#39)

--- .github/workflows/rtools40-build.sh
+++ .github/workflows/rtools40-build.sh
@@ -10,8 +10,38 @@ set -x
 # Absolute path to this script
 srcdir=$(dirname $(realpath $0))
 
+# Substitute some ucrt64 specific inputs
+if [ "$toolchain" = "ucrt64" ]; then
+sed -i 's|/mingw|/ucrt|g' "${srcdir}/MkRules.local.in"
+sed -i 's|/mingw64|/ucrt64|g' "${srcdir}/create-tcltk-bundle.sh"
+sed -i 's|,x86_64|,ucrt-x86_64|g' "${srcdir}/create-tcltk-bundle.sh"
+fi
+
+# Temp fix for older rtools40 installations
+if ! grep -Fq "ucrt64" /etc/pacman.conf; then
+echo "[ucrt64]" >> /etc/pacman.conf
+echo "Server = https://cran.r-project.org/bin/windows/Rtools/4.0/ucrt64/" >> /etc/pacman.conf
+echo "SigLevel = Never" >> /etc/pacman.conf
+fi
+
+# Get architecture
+case ${toolchain} in
+  mingw32)
+    _arch=i686
+    WIN=32
+  ;;
+  mingw64)
+    _arch=x86_64
+    WIN=64
+  ;;
+  ucrt64)
+    _arch=ucrt-x86_64
+    WIN=64
+  ;;
+esac
+
 # Put pdflatex on the path (needed only for CMD check)
-export PATH="$PATH:/c/progra~1/git/bin:/c/progra~1/MiKTeX/miktex/bin/x64"
+export PATH="$PATH:$HOME/AppData/Local/Programs/MiKTeX/miktex/bin/x64:/c/progra~1/MiKTeX/miktex/bin/x64:/c/progra~1/MiKTeX 2.9/miktex/bin/x64"
 echo "PATH: $PATH"
 pdflatex --version
 texindex --version
@@ -27,8 +57,8 @@ curl -sSL https://curl.haxx.se/ca/cacert.pem > etc/curl-ca-bundle.crt
 ./.github/workflows/svn-info.sh
 
 # Install system libs
-pacman -Syu --noconfirm
-pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran,icu,libtiff,libjpeg,libpng,pcre2,xz,bzip2,zlib,cairo,tk,curl}
+pacman -Sy --noconfirm
+pacman -S --needed --noconfirm mingw-w64-${_arch}-{gcc,gcc-fortran,icu,libtiff,libjpeg,libpng,pcre2,xz,bzip2,zlib,cairo,tk,curl}
 
 # Create the TCL bundle required by tcltk package
 mkdir -p Tcl/{bin,bin64,lib,lib64}
