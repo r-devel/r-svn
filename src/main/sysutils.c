d667ed6f91d690d4b4a199376ebef8b0eaa8e949
brodieG <brodieG@users.noreply.github.com>
2021-08-22 20:25:41 -0400

    fix bad encoding handing in wtransChar

--- src/main/sysutils.c
+++ src/main/sysutils.c
@@ -1301,8 +1301,14 @@ next_char:
 	    R_AllocStringBuffer(2*cbuff.bufsize, &cbuff);
 	    goto top_of_loop;
 	}
-	snprintf(outbuf, 5, "<%02x>", (unsigned char)*inbuf);
-	outbuf += 4; outb -= 4;
+	union c_or_wc {
+	    char * c;
+	    wchar_t * wc;
+	};
+	union c_or_wc ob;
+	ob.c = outbuf;
+	swprintf(ob.wc, 5, L"<%02x>", (unsigned char)*inbuf);
+	outbuf += 4 * sizeof(wchar_t); outb -= 4 * sizeof(wchar_t);
 	inbuf++; inb--;
 	goto next_char;
 	/* if(!knownEnc) Riconv_close(obj);
