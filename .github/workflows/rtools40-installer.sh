f2e019a6b9ce4df40d2df6d0861118feb1f32c90
Jeroen Ooms <jeroenooms@gmail.com>
2021-08-31 21:13:02 +0200

    Only build 64-bit windows installers

--- .github/workflows/rtools40-installer.sh
+++ .github/workflows/rtools40-installer.sh
@@ -29,20 +29,14 @@ curl -sSL https://curl.se/ca/cacert.pem > etc/curl-ca-bundle.crt
 
 # Install system libs
 pacman -Syu --noconfirm
-pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran,icu,libtiff,libjpeg,libpng,pcre2,xz,bzip2,zlib,cairo,tk,curl}
+pacman -S --needed --noconfirm mingw-w64-x86_64-{gcc,gcc-fortran,icu,libtiff,libjpeg,libpng,pcre2,xz,bzip2,zlib,cairo,tk,curl}
 
 # Create the TCL bundle required by tcltk package
 mkdir -p Tcl/{bin,bin64,lib,lib64}
 ${scripts}/create-tcltk-bundle.sh
 
-# Copy source dir and build 32-bit R
-MSYS="winsymlinks:lnk" cp -Rf "." "../build32"
-cd "../build32/src/gnuwin32"
-sed -e "s|@win@|32|" -e "s|@texindex@||" -e "s|@home32@||" "${scripts}/MkRules.local.in" > MkRules.local
-make 32-bit
-
 # Build 64 bit + docs and installers
 cd "${sources}/src/gnuwin32"
 TEXINDEX=$(cygpath -m $(which texindex))  
-sed -e "s|@win@|64|" -e "s|@texindex@|${TEXINDEX}|" -e "s|@home32@|${sources}/../build32|" "${scripts}/MkRules.local.in" > MkRules.local
+sed -e "s|@win@|64|" -e "s|@texindex@|${TEXINDEX}|" -e "s|@home32@||" "${scripts}/MkRules.local.in" > MkRules.local
 make distribution
