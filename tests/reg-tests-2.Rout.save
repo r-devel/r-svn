r-devel/r-svn

commit 97cee505eaeeacb5c62c9e5ba35271bb2fe593e4

deparse() of !!
git-svn-id: https://svn.r-project.org/R/trunk@74540 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/deparse.c
@@ -567,7 +567,8 @@ static Rboolean needsparens(PPinfo mainop, SEXP arg, unsigned int left)
 			if (arginfo.precedence == PREC_SUM)   /* binary +/- precedence upgraded as unary */
 			    arginfo.precedence = PREC_SIGN;
 		    case 2:
-			if (mainop.precedence == PREC_COMPARE && arginfo.precedence == PREC_COMPARE)
+			if (mainop.precedence == PREC_COMPARE &&
+			    arginfo.precedence == PREC_COMPARE)
 		          return TRUE;     /*   a < b < c   is not legal syntax */
 			break;
 		    default:
@@ -581,6 +582,9 @@ static Rboolean needsparens(PPinfo mainop, SEXP arg, unsigned int left)
 		case PP_ASSIGN2:
 		case PP_UNARY:
 		case PP_DOLLAR:
+		    /* Same as other unary operators above */
+		    if (arginfo.precedence == PREC_NOT && !left)
+			return FALSE;
 		    if (mainop.precedence > arginfo.precedence
 			|| (mainop.precedence == arginfo.precedence && left == mainop.rightassoc)) {
 			return TRUE;

tests/reg-tests-2.R
@@ -2998,6 +2998,15 @@ a <- alist(one = 1, two = )
 dput(a)
 ## deparsed two to quote()
 
+## Deparsing of repeated unary operators; the first 3 were "always" ok:
+quote(~~x)
+quote(++x)
+quote(--x)
+quote(!!x) # was `!(!x)`
+quote(??x) # Suboptimal
+quote(~+-!?x) # ditto: ....`?`(x)
+## `!` no longer produces parentheses now
+
 
 ## summary.data.frame() with NAs in columns of class "Date" -- PR#16709
 x <- c(18000000, 18810924, 19091227, 19027233, 19310526, 19691228, NA)

tests/reg-tests-2.Rout.save
@@ -1,5 +1,5 @@
 
-R Under development (unstable) (2018-03-29 r74484) -- "Unsuffered Consequences"
+R Under development (unstable) (2018-04-05 r74535) -- "Unsuffered Consequences"
 Copyright (C) 2018 The R Foundation for Statistical Computing
 Platform: x86_64-pc-linux-gnu (64-bit)
 
@@ -7714,7 +7714,7 @@ a + b < c + d
 > quote(`!`(a < b))
 !a < b
 > quote(`<`(!b, !d))
-(!b) < (!d)
+(!b) < !d
 > quote(`&`(!b, !d))
 !b & !d
 > quote(`!`(a & b))
@@ -7753,6 +7753,21 @@ b = `?`(d)
 > dput(a)
 list(one = 1, two = )
 > ## deparsed two to quote()
+> 
+> ## Deparsing of repeated unary operators; the first 3 were "always" ok:
+> quote(~~x)
+~~x
+> quote(++x)
+++x
+> quote(--x)
+--x
+> quote(!!x) # was `!(!x)`
+!!x
+> quote(??x) # Suboptimal
+`?`(`?`(x))
+> quote(~+-!?x) # ditto: ....`?`(x)
+~+-!`?`(x)
+> ## `!` no longer produces parentheses now
 > 
 > 
 > ## summary.data.frame() with NAs in columns of class "Date" -- PR#16709

commit 47517cbb26e4c743130e4656050b093ce44d9309

fix `!(.)` deparsing (broken by r74540 which fixed `!!` deparsing)
git-svn-id: https://svn.r-project.org/R/trunk@81845 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/deparse.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1997--2021  The R Core Team
+ *  Copyright (C) 1997--2022  The R Core Team
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -622,16 +622,14 @@ static Rboolean needsparens(PPinfo mainop, SEXP arg, unsigned int left,
 		case PP_ASSIGN:
 		case PP_ASSIGN2:
 		case PP_DOLLAR:
-		    /* Same as other unary operators above */
-		    if (arginfo.precedence == PREC_NOT && !left)
-			return FALSE;
 		    if (mainop.precedence > arginfo.precedence
 			|| (mainop.precedence == arginfo.precedence && left == mainop.rightassoc)) {
 			return TRUE;
 		    }
 		    break;
 		case PP_UNARY:
-		    return left && mainop.precedence > arginfo.precedence;
+		    return (left && mainop.precedence > arginfo.precedence)
+			|| (deepLeft && deepLeft > arginfo.precedence);
 		case PP_FOR:
 		case PP_IF:
 		case PP_WHILE:
@@ -1303,7 +1301,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		    	print2buff("(", d);
 		    if ((parens = needsparens(fop, CAR(s), 1, prevLeft)))
 			print2buff("(", d);
-		    d->left = !parens;
+		    d->left = parens ? 0 : fop.precedence;
 		    deparse2buff(CAR(s), d);
 		    if (parens)
 			print2buff(")", d);
@@ -1312,7 +1310,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		    print2buff(" ", d);
 		    if ((parens = needsparens(fop, CADR(s), 0, prevLeft)))
 			print2buff("(", d);
-		    d->left = prevLeft && !parens;
+		    d->left = parens ? 0 : prevLeft;
 		    deparse2buff(CADR(s), d);
 		    if (parens)
 			print2buff(")", d);
@@ -1324,7 +1322,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		case PP_DOLLAR:
 		    if ((parens = needsparens(fop, CAR(s), 1, prevLeft)))
 			print2buff("(", d);
-		    d->left = !parens;
+		    d->left = parens ? 0 : fop.precedence;
 		    deparse2buff(CAR(s), d);
 		    if (parens)
 			print2buff(")", d);
@@ -1336,7 +1334,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		    else {
 			if ((parens = needsparens(fop, CADR(s), 0, prevLeft)))
 			    print2buff("(", d);
-			d->left = prevLeft && !parens;
+			d->left = parens ? 0 : prevLeft;
 			deparse2buff(CADR(s), d);
 			if (parens)
 			    print2buff(")", d);
@@ -1346,7 +1344,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		case PP_BINARY:
 		    if ((parens = needsparens(fop, CAR(s), 1, prevLeft)))
 			print2buff("(", d);
-		    d->left = !parens;
+		    d->left = parens ? 0 : fop.precedence;
 		    deparse2buff(CAR(s), d);
 		    if (parens)
 			print2buff(")", d);
@@ -1357,7 +1355,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 
 		    if ((parens = needsparens(fop, CADR(s), 0, prevLeft)))
 			print2buff("(", d);
-		    d->left = prevLeft && !parens;
+		    d->left = parens ? 0 : prevLeft;
 		    deparse2buff(CADR(s), d);
 		    if (parens)
 			print2buff(")", d);
@@ -1370,7 +1368,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		case PP_BINARY2:	/* no space between op and args */
 		    if ((parens = needsparens(fop, CAR(s), 1, prevLeft)))
 			print2buff("(", d);
-		    d->left = !parens;
+		    d->left = parens ? 0 : fop.precedence;
 		    deparse2buff(CAR(s), d);
 		    if (parens)
 			print2buff(")", d);
@@ -1378,7 +1376,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		    print2buff(CHAR(PRINTNAME(op)), d); /* ASCII */
 		    if ((parens = needsparens(fop, CADR(s), 0, prevLeft)))
 			print2buff("(", d);
-		    d->left = prevLeft && !parens;
+		    d->left = parens ? 0 : prevLeft;
 		    deparse2buff(CADR(s), d);
 		    if (parens)
 			print2buff(")", d);
@@ -1388,7 +1386,7 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		    print2buff(CHAR(PRINTNAME(op)), d); /* ASCII */
 		    if ((parens = needsparens(fop, CAR(s), 0, prevLeft)))
 			print2buff("(", d);
-		    d->left = prevLeft;
+		    d->left = parens ? 0 : prevLeft;
 		    deparse2buff(CAR(s), d);
 		    if (parens)
 			print2buff(")", d);

tests/reg-tests-2.R
@@ -3271,3 +3271,16 @@ quote(`$`(`$`(1, if(L) 2), 3))
 quote(`$`(1 + repeat 2, 3))
 quote(`=`(`$`(1, `$`(2, repeat 3)), 4))
 ## these were really bad in  R <= 4.1.x
+
+
+## Deparsing of !  -- PR#18284
+## no parens in 3.5.0 <= R <= 4.1.x:
+quote(1 +  `!`(2) + 3) -> x; x
+quote(1 + +`!`(2) + 3)
+quote(1 + `!`(!2) + 3)
+quote(1 + `!`(if(L) 2) + 3)
+## ok in 3.5.0 <= R <= 4.1.x:
+quote(`&`(a < !b, d))
+## deparse--parse roundtrip is stable (basically)
+stopifnot(eval(x) == 4, eval(parse(text = deparse(x))) == 4)
+## eval()ed to 1 since R 3.5.0 {also because of the weak precedence of `!`}

tests/reg-tests-2.Rout.save
@@ -1,5 +1,5 @@
 
-R Under development (unstable) (2022-01-03 r81435) -- "Unsuffered Consequences"
+R Under development (unstable) (2022-02-22 r81797) -- "Unsuffered Consequences"
 Copyright (C) 2022 The R Foundation for Statistical Computing
 Platform: aarch64-apple-darwin21.2.0 (64-bit)
 
@@ -8233,4 +8233,22 @@ a$b
 > quote(`=`(`$`(1, `$`(2, repeat 3)), 4))
 `$`(1, `$`(2, repeat 3)) = 4
 > ## these were really bad in  R <= 4.1.x
+> 
+> 
+> ## Deparsing of !  -- PR#18284
+> ## no parens in 3.5.0 <= R <= 4.1.x:
+> quote(1 +  `!`(2) + 3) -> x; x
+1 + (!2) + 3
+> quote(1 + +`!`(2) + 3)
+1 + +(!2) + 3
+> quote(1 + `!`(!2) + 3)
+1 + (!!2) + 3
+> quote(1 + `!`(if(L) 2) + 3)
+1 + (!if (L) 2) + 3
+> ## ok in 3.5.0 <= R <= 4.1.x:
+> quote(`&`(a < !b, d))
+a < !b & d
+> ## deparse--parse roundtrip is stable (basically)
+> stopifnot(eval(x) == 4, eval(parse(text = deparse(x))) == 4)
+> ## eval()ed to 1 since R 3.5.0 {also because of the weak precedence of `!`}
 > 