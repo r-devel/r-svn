588bb6aa926dab3b7798539e9f819cbfeb3781b0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-11-13 17:22:57 +0000

    back out r87328 for now
    git-svn-id: https://svn.r-project.org/R/trunk@87329 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/install.R
+++ src/library/tools/R/install.R
@@ -2762,8 +2762,6 @@ if(FALSE) {
                         paste0("LTO_FC=", shQuote("$(LTO_FC_OPT)")))
                   else if(isFALSE(use_lto)) c("LTO=", "LTO_FC=")
                   )
-    if(config_val_to_logical(Sys.getenv("_R_USE_NO_REMAP_", "TRUE")))
-         makeargs <- c(makeargs, "XDEFS=-DR_NO_REMAP")
     if(config_val_to_logical(Sys.getenv("_R_CXX_USE_NO_REMAP_", "TRUE")))
          makeargs <- c(makeargs, "CXX_DEFS=-DR_NO_REMAP")
 ##    if(config_val_to_logical(Sys.getenv("_R_USE_STRICT_R_HEADERS_", "FALSE")))
