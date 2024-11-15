bee47384fd7bb6947f98d5c65c2975074b86e9ad
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-11-15 07:38:34 +0000

    revert unfinished parts of 87334
    git-svn-id: https://svn.r-project.org/R/trunk@87335 00db46b3-68df-0310-9c12-caf00c1e9a41

--- doc/manual/R-exts.c
+++ doc/manual/R-exts.c
@@ -1,7 +1,7 @@
 /*
      C code of the .Call/.External examples in `Writing R extensions'
      Compile with R CMD SHLIB R-exts.c
-     Then use the R code in R-exts.R
+     The use the R code in R-exts.R
  */
 
 
@@ -13,8 +13,8 @@
 /* second version */
 SEXP out(SEXP x, SEXP y)
 {
-    int nx = Rf_length(x), ny = Rf_length(y);
-    SEXP ans = PROTECT(Rf_allocMatrix(REALSXP, nx, ny));
+    int nx = length(x), ny = length(y);
+    SEXP ans = PROTECT(allocMatrix(REALSXP, nx, ny));
     double *rx = REAL(x), *ry = REAL(y), *rans = REAL(ans);
 
     for(int i = 0; i < nx; i++) {
@@ -23,10 +23,10 @@ SEXP out(SEXP x, SEXP y)
 	    rans[i + nx*j] = tmp * ry[j];
     }
 
-    SEXP dimnames = PROTECT(Rf_allocVector(VECSXP, 2));
-    SET_VECTOR_ELT(dimnames, 0, Rf_getAttrib(x, R_NamesSymbol));
-    SET_VECTOR_ELT(dimnames, 1, Rf_getAttrib(y, R_NamesSymbol));
-    Rf_setAttrib(ans, R_DimNamesSymbol, dimnames);
+    SEXP dimnames = PROTECT(allocVector(VECSXP, 2));
+    SET_VECTOR_ELT(dimnames, 0, getAttrib(x, R_NamesSymbol));
+    SET_VECTOR_ELT(dimnames, 1, getAttrib(y, R_NamesSymbol));
+    setAttrib(ans, R_DimNamesSymbol, dimnames);
     UNPROTECT(2);
     return ans;
 }
@@ -35,9 +35,9 @@ SEXP out(SEXP x, SEXP y)
 
 SEXP getListElement(SEXP list, const char *str)
 {
-    SEXP elmt = R_NilValue, names = Rf_getAttrib(list, R_NamesSymbol);
+    SEXP elmt = R_NilValue, names = getAttrib(list, R_NamesSymbol);
 
-    for (int i = 0; i < Rf_length(list); i++)
+    for (int i = 0; i < length(list); i++)
 	if(strcmp(CHAR(STRING_ELT(names, i)), str) == 0) {
 	    elmt = VECTOR_ELT(list, i);
 	    break;
@@ -49,11 +49,11 @@ SEXP getvar(SEXP name, SEXP rho)
 {
     SEXP ans;
 
-    if(!Rf_isString(name) || Rf_length(name) != 1)
-	Rf_error("name is not a single string");
-    if(!Rf_isEnvironment(rho))
-	Rf_error("rho should be an environment");
-    ans = Rf_findVar(Rf_installChar(STRING_ELT(name, 0)), rho);
+    if(!isString(name) || length(name) != 1)
+	error("name is not a single string");
+    if(!isEnvironment(rho))
+	error("rho should be an environment");
+    ans = findVar(installChar(STRING_ELT(name, 0)), rho);
     Rprintf("first value is %f\n", REAL(ans)[0]);
     return R_NilValue;
 }
@@ -67,10 +67,10 @@ SEXP convolve2(SEXP a, SEXP b)
     double *xa, *xb, *xab;
     SEXP ab;
 
-    a = PROTECT(Rf_coerceVector(a, REALSXP));
-    b = PROTECT(Rf_coerceVector(b, REALSXP));
-    na = Rf_length(a); nb = Rf_length(b); nab = na + nb - 1;
-    ab = PROTECT(Rf_allocVector(REALSXP, nab));
+    a = PROTECT(coerceVector(a, REALSXP));
+    b = PROTECT(coerceVector(b, REALSXP));
+    na = length(a); nb = length(b); nab = na + nb - 1;
+    ab = PROTECT(allocVector(REALSXP, nab));
     xa = REAL(a); xb = REAL(b); xab = REAL(ab);
     for(int i = 0; i < nab; i++) xab[i] = 0.0;
     for(int i = 0; i < na; i++)
@@ -87,10 +87,10 @@ SEXP convolveE(SEXP args)
     double *xa, *xb, *xab;
     SEXP a, b, ab;
 
-    a = PROTECT(Rf_coerceVector(CADR(args), REALSXP));
-    b = PROTECT(Rf_coerceVector(CADDR(args), REALSXP));
-    na = Rf_length(a); nb = Rf_length(b); nab = na + nb - 1;
-    ab = PROTECT(Rf_allocVector(REALSXP, nab));
+    a = PROTECT(coerceVector(CADR(args), REALSXP));
+    b = PROTECT(coerceVector(CADDR(args), REALSXP));
+    na = length(a); nb = length(b); nab = na + nb - 1;
+    ab = PROTECT(allocVector(REALSXP, nab));
     xa = REAL(a); xb = REAL(b); xab = REAL(ab);
     for(int i = 0; i < nab; i++) xab[i] = 0.0;
     for(int i = 0; i < na; i++)
@@ -106,9 +106,9 @@ SEXP showArgs(SEXP args)
     args = CDR(args); /* skip 'name' */
     for(int i = 0; args != R_NilValue; i++, args = CDR(args)) {
         const char *name = 
-            Rf_isNull(TAG(args)) ? "" : CHAR(PRINTNAME(TAG(args)));
+            isNull(TAG(args)) ? "" : CHAR(PRINTNAME(TAG(args)));
 	SEXP el = CAR(args);
-	if (Rf_length(el) == 0) {
+	if (length(el) == 0) {
 	    Rprintf("[%d] '%s' R type, length 0\n", i+1, name);
 	    continue;
 	}
@@ -141,12 +141,12 @@ SEXP showArgs1(SEXP largs)
 {
     int i, nargs = LENGTH(largs);
     Rcomplex cpl;
-    SEXP el, names = Rf_getAttrib(largs, R_NamesSymbol);
+    SEXP el, names = getAttrib(largs, R_NamesSymbol);
     const char *name;
 
     for(i = 0; i < nargs; i++) {
 	el = VECTOR_ELT(largs, i);
-	name = Rf_isNull(names) ? "" : CHAR(STRING_ELT(names, i));
+	name = isNull(names) ? "" : CHAR(STRING_ELT(names, i));
 	switch(TYPEOF(el)) {
 	case REALSXP:
 	    Rprintf("[%d] '%s' %f\n", i+1, name, REAL(el)[0]);
@@ -174,36 +174,36 @@ SEXP showArgs1(SEXP largs)
 
 SEXP lapply(SEXP list, SEXP expr, SEXP rho)
 {
-    int n = Rf_length(list);
+    int n = length(list);
     SEXP ans;
 
-    if(!Rf_isNewList(list)) Rf_error("'list' must be a list");
-    if(!Rf_isEnvironment(rho)) Rf_error("'rho' should be an environment");
-    ans = PROTECT(Rf_allocVector(VECSXP, n));
+    if(!isNewList(list)) error("'list' must be a list");
+    if(!isEnvironment(rho)) error("'rho' should be an environment");
+    ans = PROTECT(allocVector(VECSXP, n));
     for(int i = 0; i < n; i++) {
-	Rf_defineVar(Rf_install("x"), VECTOR_ELT(list, i), rho);
-	SET_VECTOR_ELT(ans, i, Rf_eval(expr, rho));
+	defineVar(install("x"), VECTOR_ELT(list, i), rho);
+	SET_VECTOR_ELT(ans, i, eval(expr, rho));
     }
-    Rf_setAttrib(ans, R_NamesSymbol, Rf_getAttrib(list, R_NamesSymbol));
+    setAttrib(ans, R_NamesSymbol, getAttrib(list, R_NamesSymbol));
     UNPROTECT(1);
     return ans;
 }
 
 SEXP lapply2(SEXP list, SEXP fn, SEXP rho)
 {
-    int n = Rf_length(list);
+    int n = length(list);
     SEXP R_fcall, ans;
 
-    if(!Rf_isNewList(list)) Rf_error("'list' must be a list");
-    if(!Rf_isFunction(fn)) Rf_error("'fn' must be a function");
-    if(!Rf_isEnvironment(rho)) Rf_error("'rho' should be an environment");
-    R_fcall = PROTECT(Rf_lang2(fn, R_NilValue));
-    ans = PROTECT(Rf_allocVector(VECSXP, n));
+    if(!isNewList(list)) error("'list' must be a list");
+    if(!isFunction(fn)) error("'fn' must be a function");
+    if(!isEnvironment(rho)) error("'rho' should be an environment");
+    R_fcall = PROTECT(lang2(fn, R_NilValue));
+    ans = PROTECT(allocVector(VECSXP, n));
     for(int i = 0; i < n; i++) {
 	SETCADR(R_fcall, VECTOR_ELT(list, i));
-	SET_VECTOR_ELT(ans, i, Rf_eval(R_fcall, rho));
+	SET_VECTOR_ELT(ans, i, eval(R_fcall, rho));
     }
-    Rf_setAttrib(ans, R_NamesSymbol, Rf_getAttrib(list, R_NamesSymbol));
+    setAttrib(ans, R_NamesSymbol, getAttrib(list, R_NamesSymbol));
     UNPROTECT(2);
     return ans;
 }
@@ -213,7 +213,7 @@ SEXP lapply2(SEXP list, SEXP fn, SEXP rho)
 SEXP mkans(double x)
 {
     SEXP ans;
-    ans = PROTECT(Rf_allocVector(REALSXP, 1));
+    ans = PROTECT(allocVector(REALSXP, 1));
     REAL(ans)[0] = x;
     UNPROTECT(1);
     return ans;
@@ -221,8 +221,8 @@ SEXP mkans(double x)
 
 double feval(double x, SEXP f, SEXP rho)
 {
-    Rf_defineVar(Rf_install("x"), mkans(x), rho);
-    return REAL(Rf_eval(f, rho))[0];
+    defineVar(install("x"), mkans(x), rho);
+    return REAL(eval(f, rho))[0];
 }
 
 SEXP zero(SEXP f, SEXP guesses, SEXP stol, SEXP rho)
@@ -231,16 +231,16 @@ SEXP zero(SEXP f, SEXP guesses, SEXP stol, SEXP rho)
            tol = REAL(stol)[0];
     double f0, f1, fc, xc;
 
-    if(tol <= 0.0) Rf_error("non-positive tol value");
+    if(tol <= 0.0) error("non-positive tol value");
     f0 = feval(x0, f, rho); f1 = feval(x1, f, rho);
     if(f0 == 0.0) return mkans(x0);
     if(f1 == 0.0) return mkans(x1);
-    if(f0*f1 > 0.0) Rf_error("x[0] and x[1] have the same sign");
+    if(f0*f1 > 0.0) error("x[0] and x[1] have the same sign");
     for(;;) {
         xc = 0.5*(x0+x1);
-        if(fabs(x0-x1) < tol) return mkans(xc);
+        if(fabs(x0-x1) < tol) return  mkans(xc);
         fc = feval(xc, f, rho);
-        if(fc == 0) return mkans(xc);
+        if(fc == 0) return  mkans(xc);
         if(f0*fc > 0.0) {
             x0 = xc; f0 = fc;
         } else {
@@ -263,32 +263,32 @@ SEXP numeric_deriv(SEXP args)
     int i, start;
 
     expr = CADR(args);
-    if(!Rf_isString(theta = CADDR(args)))
-	Rf_error("theta should be of type character");
-    if(!Rf_isEnvironment(rho = CADDDR(args)))
-	Rf_error("rho should be an environment");
+    if(!isString(theta = CADDR(args)))
+	error("theta should be of type character");
+    if(!isEnvironment(rho = CADDDR(args)))
+	error("rho should be an environment");
 
-    ans = PROTECT(Rf_coerceVector(Rf_eval(expr, rho), REALSXP));
-    gradient = PROTECT(Rf_allocMatrix(REALSXP, LENGTH(ans), LENGTH(theta)));
+    ans = PROTECT(coerceVector(eval(expr, rho), REALSXP));
+    gradient = PROTECT(allocMatrix(REALSXP, LENGTH(ans), LENGTH(theta)));
     rgr = REAL(gradient); rans = REAL(ans);
 
     for(i = 0, start = 0; i < LENGTH(theta); i++, start += LENGTH(ans)) {
-	PROTECT(par = Rf_findVar(Rf_installChar(STRING_ELT(theta, i)), rho));
+	PROTECT(par = findVar(installChar(STRING_ELT(theta, i)), rho));
 	tt = REAL(par)[0];
 	xx = fabs(tt);
 	delta = (xx < 1) ? eps : xx*eps;
 	REAL(par)[0] += delta;
-	ans1 = PROTECT(Rf_coerceVector(Rf_eval(expr, rho), REALSXP));
+	ans1 = PROTECT(coerceVector(eval(expr, rho), REALSXP));
 	for(int j = 0; j < LENGTH(ans); j++)
             rgr[j + start] = (REAL(ans1)[j] - rans[j])/delta;
 	REAL(par)[0] = tt;
 	UNPROTECT(2); /* par, ans1 */
     }
 
-    dimnames = PROTECT(Rf_allocVector(VECSXP, 2));
+    dimnames = PROTECT(allocVector(VECSXP, 2));
     SET_VECTOR_ELT(dimnames, 1,  theta);
-    Rf_dimnamesgets(gradient, dimnames);
-    Rf_setAttrib(ans, Rf_install("gradient"), gradient);
+    dimnamesgets(gradient, dimnames);
+    setAttrib(ans, install("gradient"), gradient);
     UNPROTECT(3); /* ans  gradient  dimnames */
     return ans;
 }
