89f682b6f0adcead74a6b84d01515d4a67719f33
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-11-18 09:33:49 +0000

    sunspot.month update to Oct.2024; keep old as `sunspot.m2014`
    git-svn-id: https://svn.r-project.org/R/trunk@87340 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/datasets.R
+++ tests/datasets.R
@@ -2,7 +2,7 @@
 
 options(useFancyQuotes=FALSE)
 env <- as.environment("package:datasets")
-d <- ls(env) # don't want .names
+d <- setdiff(ls(env), "sunspot.month") # don't want .names, nor "changing" one
 for(f in d) {
     cat("\n** structure of dataset ", f, "\n", sep="")
     str(get(f, envir=env, inherits=FALSE))
