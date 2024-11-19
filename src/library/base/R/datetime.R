8953cbce0adb0abdeca36aeb18014d78a2f05c4d
Dirk Eddelbuettel <edd@debian.org>
2024-11-19 09:31:16 -0600

    Simplify format.POSIXlt() to improve bugzilla issue 17350
    there is also a bit of whitespace change as the indentation was off for under
    Emacs/ESS which is unusual for base R code.

--- src/library/base/R/datetime.R
+++ src/library/base/R/datetime.R
@@ -384,19 +384,12 @@ format.POSIXlt <- function(x, format = "", usetz = FALSE,
     if(!inherits(x, "POSIXlt")) stop("wrong class")
     if(any(f0 <- format == "")) {
         ## need list [ method here.
-    times <- unlist(unclass(x)[1L:3L])[f0]
-    secs <- x$sec[f0]; secs <- secs[is.finite(secs)]
+        times <- unlist(unclass(x)[1L:3L])[f0]
         np <- if(is.null(digits)) 0L else min(6L, digits)
-        if(np >= 1L) # no unnecessary trailing '0' :
-            for (i in seq_len(np)- 1L)
-                if(all( abs(secs - round(secs, i)) < 1e-6 )) {
-                    np <- i
-                    break
-                }
-    format[f0] <-
-        if(all(times[is.finite(times)] == 0)) "%Y-%m-%d"
-        else if(np == 0L) "%Y-%m-%d %H:%M:%S"
-        else paste0("%Y-%m-%d %H:%M:%OS", np)
+        format[f0] <-
+            if(all(times[is.finite(times)] == 0)) "%Y-%m-%d"
+            else if(np == 0L) "%Y-%m-%d %H:%M:%S"
+            else paste0("%Y-%m-%d %H:%M:%OS", np)
     }
     .Internal(format.POSIXlt(x, format, usetz))
 }
