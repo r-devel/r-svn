ed0d98bc26d5724cbaaa7bd03760671746c79eb2
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-11-21 10:31:48 +0000

    fix format.POSIXlt() for fractional secs
    git-svn-id: https://svn.r-project.org/R/trunk@87354 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/datetime.R
+++ src/library/base/R/datetime.R
@@ -382,18 +382,20 @@ format.POSIXlt <- function(x, format = "", usetz = FALSE,
                            digits = getOption("digits.secs"), ...)
 {
     if(!inherits(x, "POSIXlt")) stop("wrong class")
-    if(any(f0 <- format == "")) {
-        ## need list [ method here.
-    times <- unlist(unclass(x)[1L:3L])[f0]
-    secs <- x$sec[f0]; secs <- secs[is.finite(secs)]
-        np <- if(is.null(digits)) 0L else min(6L, digits)
-        if(np >= 1L) # no unnecessary trailing '0' :
-            for (i in seq_len(np)- 1L)
-                if(all( abs(secs - round(secs, i)) < 1e-6 )) {
+    if(any(f0 <- format == "" | grepl("%OS$", format))) {
+        if(!is.null(digits)) {
+            secs <- x$sec[f0]; secs <- secs[is.finite(secs)]
+            np <- min(6L, digits)
+            ## no unnecessary trailing '0' ; use trunc() as .Internal() code:
+            for(i in seq_len(np)- 1L)
+                if(all( abs(secs - trunc(secs*(ti <- 10^i))/ti) < 1e-6 )) {
                     np <- i
                     break
                 }
-    format[f0] <-
+        } else np <- 0L
+        ## need list `[` method here to get 1:3 ~ {sec, min, hour}:
+        times <- unlist(`names<-`(unclass(x)[1L:3L], NULL))[f0]
+       format[f0] <-
         if(all(times[is.finite(times)] == 0)) "%Y-%m-%d"
         else if(np == 0L) "%Y-%m-%d %H:%M:%S"
         else paste0("%Y-%m-%d %H:%M:%OS", np)
