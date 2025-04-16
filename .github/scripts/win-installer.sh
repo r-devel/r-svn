908db33b6d1f765976a78b5c205dc1481129fa1e
Jeroen Ooms <jeroenooms@gmail.com>
2025-04-16 16:49:41 +0200

    cleanup

--- .github/scripts/win-installer.sh
+++ .github/scripts/win-installer.sh
@@ -6,11 +6,15 @@ set -x
 # Put pdflatex on the path (needed only for CMD check)
 if [ "$( uname -s | grep ARM)" ]; then
 export PATH="/c/aarch64-w64-mingw32.static.posix/bin:$PATH:/c/Users/$USER/AppData/Roaming/TinyTeX/bin/windows:$HOME/AppData/Local/Programs/MiKTeX/miktex/bin/x64:/c/progra~1/MiKTeX/miktex/bin/x64:/c/progra~1/MiKTeX 2.9/miktex/bin/x64"
+tcltk="https://github.com/r-windows/rtools-chocolatey/releases/download/6536/tcltk-aarch64-6536-6492.zip"
+echo "USE_LLVM = 1" >> src/gnuwin32/MkRules.local
 clang --version
 else
 export PATH="/c/x86_64-w64-mingw32.static.posix/bin:$PATH:/c/Users/$USER/AppData/Roaming/TinyTeX/bin/windows:$HOME/AppData/Local/Programs/MiKTeX/miktex/bin/x64:/c/progra~1/MiKTeX/miktex/bin/x64:/c/progra~1/MiKTeX 2.9/miktex/bin/x64"
+tcltk="https://github.com/r-windows/rtools-chocolatey/releases/download/6536/tcltk-6536-6492.zip"
 gcc --version
 fi
+
 echo "PATH: $PATH"
 pdflatex --version
 texindex --version
@@ -27,15 +31,8 @@ curl -sSL https://curl.se/ca/cacert.pem > etc/curl-ca-bundle.crt
 ./.github/scripts/svn-info.sh
 
 # Download the TCL bundle required by tcltk package
-#curl -OL https://cran.r-project.org/bin/windows/Rtools/rtools43/files/tcltk-5493-5412.zip
-if [ "$( uname -s | grep ARM)" ]; then
-echo "USE_LLVM = 1" >> src/gnuwin32/MkRules.local
-curl -OL https://github.com/r-windows/rtools-chocolatey/releases/download/6536/tcltk-aarch64-6536-6492.zip
-unzip tcltk-aarch64-6536-6492.zip
-else
-curl -OL https://github.com/r-windows/rtools-chocolatey/releases/download/6536/tcltk-6536-6492.zip
-unzip tcltk-6536-6492.zip
-fi
+curl -o tcltk.zip -L $tcltk
+unzip -q tcltk.zip
 
 # Add custom flags to MkRules.local
 #cp .github/scripts/MkRules.local src/gnuwin32/
