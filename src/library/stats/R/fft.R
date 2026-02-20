4a5db1b25fe369689055c008bd32ce59d343b8df
Michael Chirico <chiricom@google.com>
2026-02-20 17:38:27 +0000

    fixes for convolve type="filter"

--- src/library/stats/R/fft.R
+++ src/library/stats/R/fft.R
@@ -25,22 +25,23 @@ nextn <- function(n, factors=c(2,3,5)) .Call(C_nextn, n, factors)
 convolve <- function(x, y, conj=TRUE, type=c("circular","open","filter"))
 {
     type <- match.arg(type)
-    n <- length(x)
+    nx <- length(x)
     ny <- length(y)
     Real <- is.numeric(x) && is.numeric(y)
     ## switch(type, circular = ..., )
     if(type == "circular") {
-        if(ny != n)
+        if(ny != nx)
             stop("length mismatch in convolution")
     }
     else { ## "open" or "filter": Pad with zeros
+        if (type == "filter" && ny>n) return(numeric())
         n1 <- ny - 1
         x <- c(rep.int(0, n1), x)
-        n <- length(y <- c(y, rep.int(0, n - 1)))# n = nx+ny-1
+        n <- length(y <- c(y, rep.int(0, nx - 1)))# n = nx+ny-1
     }
     x <- fft(fft(x)* (if(conj)Conj(fft(y)) else fft(y)), inverse=TRUE)
     if(type == "filter")
-        (if(Real) Re(x) else x)[-c(1L:n1, (n-n1+1L):n)]/n
+        (if(Real) Re(x) else x)[ny:nx]/n
     else
         (if(Real) Re(x) else x)/n
 }
