fc8e4a574e82adb3686349e29ec603ed7665ff4d
Michael Chirico <chiricom@google.com>
2026-02-20 17:50:48 +0000

    need all.equal

--- src/library/stats/tests/fft.R
+++ src/library/stats/tests/fft.R
@@ -1,9 +1,9 @@
 stopifnot(exprs = {
   !length(convolve(1:5, rep(1, 7), type="filter"))
   !length(convolve(1:5, rep(1, 6), type="filter"))
-  convolve(1:5, rep(1, 5), type="filter") == 15
-  convolve(1:5, rep(1, 4), type="filter") == c(10, 14)
-  convolve(1:5, rep(1, 3), type="filter") == c(6, 9, 12)
-  convolve(1:5, rep(1, 2), type="filter") == c(3, 5, 7, 9)
-  convolve(1:5, rep(1, 1), type="filter") == c(1, 2, 3, 4, 5)
+  all.equal(convolve(1:5, rep(1, 5), type="filter"), 15)
+  all.equal(convolve(1:5, rep(1, 4), type="filter"), c(10, 14))
+  all.equal(convolve(1:5, rep(1, 3), type="filter"), c(6, 9, 12))
+  all.equal(convolve(1:5, rep(1, 2), type="filter"), c(3, 5, 7, 9))
+  all.equal(convolve(1:5, rep(1, 1), type="filter"), c(1, 2, 3, 4, 5))
 })
