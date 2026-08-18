9a5967792383a50bf5d772d5ab6c59fbeada8aff
Kevin Ushey <kevinushey@gmail.com>
2026-08-18 10:28:33 -0700

    wide-int: print wide arrays of 3+ dimensions
    printArray's slice loop was not given the wide branch printMatrix
    received, so auto-printing a wide 3-d array (constructible via dim<-)
    errored mid-output.  Format once via R_formatWideInt and print each
    slice through the string layout.

--- src/main/printarray.c
+++ src/main/printarray.c
@@ -462,6 +462,13 @@ void printArray(SEXP x, SEXP dim, int quote, int right, SEXP dimnames)
 	    nc_last = nc;
 	    nr_last = nr;
 	}
+	/* wide integers print via the string layout, as in printMatrix;
+	   format once, not per slice */
+	SEXP xw = NULL;
+	if (TYPEOF(x) == INTSXP && R_isWideInteger(x)) {
+	    PROTECT(xw = R_formatWideInt(x));
+	    nprotect++;
+	}
 	for (i = 0; i < nb_pr; i++) {
 	    bool do_ij = nb > 0,
 		i_last = (i == nb_pr - 1); /* for the last slice */
@@ -495,7 +502,11 @@ void printArray(SEXP x, SEXP dim, int quote, int right, SEXP dimnames)
 		printLogicalMatrix(x, i * b, use_nr, nr, use_nc, dn0, dn1, rn, cn, do_ij);
 		break;
 	    case INTSXP:
-		printIntegerMatrix(x, i * b, use_nr, nr, use_nc, dn0, dn1, rn, cn, do_ij);
+		if (xw != NULL)
+		    printStringMatrix (xw, i * b, use_nr, nr, use_nc,
+				       0, 1, dn0, dn1, rn, cn, do_ij);
+		else
+		    printIntegerMatrix(x, i * b, use_nr, nr, use_nc, dn0, dn1, rn, cn, do_ij);
 		break;
 	    case REALSXP:
 		printRealMatrix   (x, i * b, use_nr, nr, use_nc, dn0, dn1, rn, cn, do_ij);
